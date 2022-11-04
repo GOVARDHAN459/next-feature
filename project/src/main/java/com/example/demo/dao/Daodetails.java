67f009901c03f6eda9aa6217528a37c769aac6ec
unknown <capjusg07@iiht.tech>
2022-11-04 14:29:19 +0530

    this is final project

--- project/src/main/java/com/example/demo/dao/Daodetails.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.dao;
-
-import com.example.demo.pojo.Details;
-
-public interface Daodetails 
-{
-	public Details SaveData(Details details);
-}
--- project/src/main/java/com/example/demo/service/Service.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.pojo.Details;
-
-public interface Service
-{
-	public Details savePerson(Details details);
-}
